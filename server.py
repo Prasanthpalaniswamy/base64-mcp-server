35827b2d99188298e593c19a73280a3580395631
Prasanthpalaniswamy <prasanth.palaniswamy@gmail.com>
2026-04-18 16:33:20 +0530

    Fix for invalid header error in postman

--- server.py
+++ server.py
@@ -56,6 +56,10 @@ if __name__ == "__main__":
         #     }
         port = int(os.environ.get("PORT", 8000))
         print("MCP HTTP server starting...")
-        uvicorn.run(app, host="0.0.0.0", port=port)
+        uvicorn.run(app,
+                host="0.0.0.0",
+                port=port,
+                proxy_headers=True,
+                forwarded_allow_ips="*")
     else:
         mcp.run()
